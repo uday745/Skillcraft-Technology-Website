87f70f9618fceeedd667075a9e810f9a47d0e876
uday745 <51956625+uday745@users.noreply.github.com>
2025-08-31 00:11:05 +0530

    Update SkillcraftWebsiteApplicationTests.java

--- src/test/java/com/skillcraft/skillcraft_website/SkillcraftWebsiteApplicationTests.java
+++ src/test/java/com/skillcraft/skillcraft_website/SkillcraftWebsiteApplicationTests.java
@@ -4,24 +4,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SkillcraftWebsiteApplicationTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
+    void contextLoads() {
+        // This test just makes sure the Spring application starts correctly
+    }
+
+    @Test
     void homePageShouldReturnWelcomeMessage() {
-        String body = this.restTemplate.getForObject("http://localhost:9090/", String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
         assertThat(body).contains("Welcome to Skillcraft Technology Website!");
     }
 
     @Test
     void devopsPageShouldReturnDevOpsTopics() {
-        String body = this.restTemplate.getForObject("http://localhost:9090/devops", String.class);
+        String body = this.restTemplate.getForObject("/devops", String.class);
         assertThat(body).contains("DevOps Topics");
     }
 }
